ef19876294cccc94b9078e880d9a3ad7c912692f
Ivan Zakutnii <zakutnii.ivan@gmail.com>
2025-11-26 17:51:26 +0400

    chore: Added reflexy comments for methods in main module

--- linked_list_of_integers/funcs.go
+++ linked_list_of_integers/funcs.go
@@ -1,19 +1,25 @@
 package linked_list
 
+// This is a naive solution for the additional task because due to the mentioned weakness
+// of the linked list interface we have to access implementation details directly.
+// This situation could be improved by adding getters for current value and next node to Node.
+// Alternatively, an iterator interface could be implemented for the list itself with Next and HasNext methods.
+//
+// Complexity: O(n) for both time and space, depends on the length of input lists.
+
 func NaiveSumLists(list1 *LinkedList, list2 *LinkedList) *LinkedList {
 	sumList := LinkedList{}
 	if list1.length != list2.length {
 		return &sumList
 	}
 
-	list1Сurrent := list1.head
-	list2Сurrent := list2.head
-	for list1Сurrent != nil && list2Сurrent != nil {
-		sumList.AddInTail(Node{value: list1Сurrent.value + list2Сurrent.value})
-		list1Сurrent = list1Сurrent.next
-		list2Сurrent = list2Сurrent.next
+	list1Current := list1.head
+	list2Current := list2.head
+	for list1Current != nil && list2Current != nil {
+		sumList.AddInTail(Node{value: list1Current.value + list2Current.value})
+		list1Current = list1Current.next
+		list2Current = list2Current.next
 	}
 
 	return &sumList
-
 }
--- linked_list_of_integers/linked_list.go
+++ linked_list_of_integers/linked_list.go
@@ -13,6 +13,7 @@ type LinkedList struct {
 	tail   *Node
 }
 
+// O(1) - tail pointer allows constant time append
 func (l *LinkedList) AddInTail(item Node) {
 	if l.head == nil {
 		l.head = &item
@@ -24,11 +25,12 @@ func (l *LinkedList) AddInTail(item Node) {
 	l.length++
 }
 
+// O(1) - length counter stored in struct
 func (l *LinkedList) Count() int {
 	return l.length
 }
 
-// error not nil, if node not found
+// O(n) - linear search until match found
 func (l *LinkedList) Find(n int) (Node, error) {
 	for current := l.head; current != nil; current = current.next {
 		if current.value == n {
@@ -38,6 +40,8 @@ func (l *LinkedList) Find(n int) (Node, error) {
 	return Node{value: -1, next: nil}, errors.New("Node not found")
 }
 
+// Time: O(n) - full list traversal
+// Space: O(k) where k is number of found elements
 func (l *LinkedList) FindAll(n int) []Node {
 	var nodes []Node
 	for current := l.head; current != nil; current = current.next {
@@ -48,6 +52,9 @@ func (l *LinkedList) FindAll(n int) []Node {
 	return nodes
 }
 
+// O(n) for any value of all - linear list traversal
+// Uses two-pointer pattern (prev, current) for singly linked list
+// since there is no way to get previous node directly.
 func (l *LinkedList) Delete(n int, all bool) {
 	for l.head != nil && l.head.value == n {
 		l.head = l.head.next
@@ -86,6 +93,7 @@ func (l *LinkedList) Delete(n int, all bool) {
 	}
 }
 
+// O(1) - pointer operations only
 func (l *LinkedList) Insert(after *Node, add Node) {
 	if l.tail == after {
 		l.tail.next = &add
@@ -100,6 +108,7 @@ func (l *LinkedList) Insert(after *Node, add Node) {
 
 }
 
+// O(1) - pointer operations only
 func (l *LinkedList) InsertFirst(first Node) {
 	if l.head == nil {
 		l.head = &first
@@ -113,6 +122,9 @@ func (l *LinkedList) InsertFirst(first Node) {
 
 }
 
+// O(1) - pointer operations only
+// Memory is freed by Go garbage collector when no references to nodes remain.
+// In languages without GC explicit traversal and deallocation of each node would be required.
 func (l *LinkedList) Clean() {
 	l.length = 0
 	l.tail = nil
